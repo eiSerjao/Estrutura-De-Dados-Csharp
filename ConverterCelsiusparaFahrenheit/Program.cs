baedb874c2d1a5c5e2c15b23ed8daae3aebf18b3
eiSerjao <paulosergiobarrosdesouza@gmail.com>
2025-04-11 19:02:03 -0300

    Adicionando um conversor de Celsius para Fahrenheit

---
+++ ConverterCelsiusparaFahrenheit/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+//Esse progama converte Celsius para Fahrenheit
+
+double converterCelsiusParaFahrenheit(double c)
+{
+    double f;
+    f = 1.8 * c + 32;
+    return f;
+}
+
+double t1;
+double t2;
+
+Console.WriteLine("Digite a temperatura em Celsius: ");
+string valorEscolhido = Console.ReadLine()!;
+
+t1 = int.Parse(valorEscolhido);
+
+t2 = converterCelsiusParaFahrenheit(t1);
+Console.WriteLine($"A temperatura em Fahrenheit é: {t2}°F");
+
