f61d88bd0c4a97911f1c048614c007b32a43c071
eiSerjao <paulosergiobarrosdesouza@gmail.com>
2025-04-12 18:13:14 -0300

    Trazendo comentarios

--- 01 - Conceitos Fundamentais/ConverterCelsiusparaFahrenheit/Program.cs
+++ 01 - Conceitos Fundamentais/ConverterCelsiusparaFahrenheit/Program.cs
@@ -5,6 +5,7 @@ using System.Text.RegularExpressions;
 
 //Esse progama converte Celsius para Fahrenheit
 
+//A fórmula de conversão é: F = 1.8 * C + 32
 double converterCelsiusParaFahrenheit(double c)
 {
     double f;
@@ -12,14 +13,18 @@ double converterCelsiusParaFahrenheit(double c)
     return f;
 }
 
+//Declaração de variáveis
 double t1;
 double t2;
 
+//Entrada de dados
 Console.WriteLine("Digite a temperatura em Celsius: ");
 string valorEscolhido = Console.ReadLine()!;
 
+//Validação de entrada
 t1 = int.Parse(valorEscolhido);
 
+//Validação da convenção de celsius para fahrenheit
 t2 = converterCelsiusParaFahrenheit(t1);
 Console.WriteLine($"A temperatura em Fahrenheit é: {t2}°F");
 
