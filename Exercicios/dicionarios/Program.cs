6a06cca122b50ce5a6ee9329e4df267980225081
eiSerjao <paulosergiobarrosdesouza@gmail.com>
2025-04-17 16:42:56 -0300

    Corrigindo erro NuGet

---
+++ Exercicios/dicionarios/Program.cs
@@ -0,0 +1,17 @@
+﻿// Criar um dicionário que represente um aluno, com uma lista de notas, e mostre a média de suas notas na tela.
+
+Dictionary<string, List<double>> alunos = new Dictionary<string, List<double>>()
+{
+    { "João", new List<double> { 7.5, 8.0, 9.0 } },
+    { "Maria", new List<double> { 6.5, 7.0, 8.5 } },
+    { "Pedro", new List<double> { 9.5, 10.0, 9.0 } }
+};
+
+foreach (var aluno in alunos)
+{
+    string nome = aluno.key;
+    List<double> notas = aluno.value;
+    double media = notas.Average();
+    
+
+}
