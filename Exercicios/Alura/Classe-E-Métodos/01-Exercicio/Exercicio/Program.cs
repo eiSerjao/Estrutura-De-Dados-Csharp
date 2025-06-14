b96e676f208227484d2585af529252be6b667afd
eiSerjao <paulosergiobarrosdesouza@gmail.com>
2025-06-14 19:47:27 -0300

    Atualizando Atributos para Propotiers

--- Exercicios/Alura/Classe-E-Métodos/01-Exercicio/Exercicio/Program.cs
+++ Exercicios/Alura/Classe-E-Métodos/01-Exercicio/Exercicio/Program.cs
@@ -3,16 +3,16 @@
 class ContaBancaria
 {
     // Atributos
-    public int numeroIndicar;      // Número identificador da conta
-    public string nomeTitular;     // Nome do titular
-    public double saldo;           // Saldo da conta
-    public string senha;           // Senha da conta
+    public int NumeroIndicar { get; set; }    // Número identificador da conta
+    public string NomeTitular { get; set; }    // Nome do titular
+    public double Saldo { get; set; }         // Saldo da conta
+    public string Senha { get; set; }           // Senha da conta
 
     // Método para exibir as informações da conta
     public void ExibirInformacoes()
     {
-        Console.WriteLine($"Titular: {nomeTitular}");
-        Console.WriteLine($"Saldo: R$ {saldo:F2}");
+        Console.WriteLine($"Titular: {NomeTitular}");
+        Console.WriteLine($"Saldo: R$ {Saldo:F2}");
     }
 }
 
@@ -24,10 +24,10 @@ class Program
         ContaBancaria conta1 = new ContaBancaria();
 
         // Atribuindo valores aos atributos
-        conta1.numeroIndicar = 123456;
-        conta1.nomeTitular = "Maria Souza";
-        conta1.saldo = 2500.75;
-        conta1.senha = "senhaSecreta";
+        conta1.NumeroIndicar = 123456;
+        conta1.NomeTitular = "Maria Souza";
+        conta1.Saldo = 2500.75;
+        conta1.Senha = "senhaSecreta";
 
         // Exibindo as informações usando o método da própria classe
         conta1.ExibirInformacoes();
