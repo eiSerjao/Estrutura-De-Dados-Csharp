0cbead250d55979e3239df11a23bea133e93d1d0
eiSerjao <paulosergiobarrosdesouza@gmail.com>
2025-06-14 20:25:23 -0300

    Atualizando: Substituir os Atributos para Propotiers e Adicioandao Propotier DescriçãoDetalahada

--- Exercicios/Alura/Classe-E-Métodos/02-Exercicio/Program.cs
+++ Exercicios/Alura/Classe-E-Métodos/02-Exercicio/Program.cs
@@ -2,21 +2,49 @@
 
 public class Carro
 {
-    public string marca;
-    public string modelo;
-    public int anoFrabicacao;
-    public string cor;
-    public string tipoCombustivel;
-    public int velocidadeMaxima;
+    public string Nome { get; set; }
+    public string Marca { get; set; }
+    public string Carroceria { get; set; }
+
+    private int _ano;
+    public int Ano
+    {
+        get
+        {
+            return _ano;
+        }
+        set
+        {
+            if (value < 1960 || value > 2025)
+            {
+                Console.WriteLine("Ano inválido. O ano deve estar entre 1960 e 2025.");
+            }
+            else
+            {
+                _ano = value;
+            }
+        }
+    }
+    public string Cor {get; set;}
+    public string TipoCombustivel {get; set;}
+    public int VelocidadeMaxima {get; set;}
+
+    public string DescricaoDetalhada
+    {
+        get
+        {
+            return $"Fabricante: {Marca}, Carroceria: {Carroceria}, Ano: {Ano}";
+        }
+    }
 
     public void ExibirInformacoes()
     {
-        Console.WriteLine($"Marca: {marca}");
-        Console.WriteLine($"Modelo: {modelo}");
-        Console.WriteLine($"Ano de Fabricação: {anoFrabicacao}");
-        Console.WriteLine($"Cor: {cor}");
-        Console.WriteLine($"Tipo de Combustível: {tipoCombustivel}");
-        Console.WriteLine($"Velocidade Máxima: {velocidadeMaxima} km/h");
+        Console.WriteLine($"Marca: {Marca}");
+        Console.WriteLine($"Modelo: {Carroceria}");
+        Console.WriteLine($"Ano de Fabricação: {Ano}");
+        Console.WriteLine($"Cor: {Cor}");
+        Console.WriteLine($"Tipo de Combustível: {TipoCombustivel}");
+        Console.WriteLine($"Velocidade Máxima: {VelocidadeMaxima} km/h");
     }
 
     public void Acelerar()
@@ -35,27 +63,34 @@ public class Carro
     }
 }
 
+public class Produto
+{
+    
+}
 class Program
 {
     static void Main()
     {
-        Carro FordKaSedan = new Carro();
+        Carro FordKa = new Carro();
+        FordKa.Nome = "Ford Ka";
+        FordKa.Marca = "Ford";
+        FordKa.Carroceria = "Sedan";
+        FordKa.Ano = 2016;
+        FordKa.Cor = "Branco";
+        FordKa.TipoCombustivel = "Gasolina & Etanol";
+        FordKa.VelocidadeMaxima = 180;
 
-        FordKaSedan.marca = "Ford";
-        FordKaSedan.modelo = "Ford Ka Sedan";
-        FordKaSedan.anoFrabicacao = 2016;
-        FordKaSedan.cor = "Branco";
-        FordKaSedan.tipoCombustivel = "Gasolina & Etanol";
-        FordKaSedan.velocidadeMaxima = 180;
+        //FordKaSedan.ExibirInformacoes();
 
-        FordKaSedan.ExibirInformacoes();
+        Console.WriteLine($"Informações do Carro {FordKa.Nome}:");
+        Console.WriteLine(FordKa.DescricaoDetalhada);
 
-        FordKaSedan.Acelerar();
+        // FordKa.Acelerar();
 
-        FordKaSedan.Freando();
+        // FordKa.Freando();
 
-        FordKaSedan.Buzinar();
+        // FordKa.Buzinar();
 
-        Console.ReadKey();
+        // Console.ReadKey();
     }
 }
